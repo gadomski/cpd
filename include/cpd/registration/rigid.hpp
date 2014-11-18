63791c8090502b8c5fde1da645671bdcd683b9f1
Pete Gadomski <pete.gadomski@gmail.com>
2014-11-18 15:25:34 -0700

    Use scaling

--- include/cpd/registration/rigid.hpp
+++ include/cpd/registration/rigid.hpp
@@ -11,8 +11,25 @@ namespace registration
 
 class Rigid : public Base
 {
+public:
+
+    explicit Rigid(
+            float tol = 1e-5,
+            int max_it = 150,
+            float outliers = 0.1,
+            bool use_fgt = false,
+            bool strict_rot = true,
+            bool use_scaling = true
+            );
+    
+    inline bool strict_rot() const { return m_strict_rot; }
+    inline bool use_scaling() const { return m_use_scaling; }
+
 private:
     virtual SpResult execute(const arma::mat& X, const arma::mat& Y) const;
+
+    bool m_strict_rot;
+    bool m_use_scaling;
 
 };
 
--- src/registration/rigid.cpp
+++ src/registration/rigid.cpp
@@ -8,6 +8,15 @@ namespace cpd
 namespace registration
 {
 
+
+Rigid::Rigid(float tol, int max_it, float outliers, bool use_fgt, bool strict_rot,
+             bool use_scaling)
+    : Base(tol, max_it, outliers, use_fgt)
+    , m_strict_rot(strict_rot)
+    , m_use_scaling(use_scaling)
+{}
+
+
 SpResult Rigid::execute(const arma::mat& X, const arma::mat& Y) const
 {
     const arma::uword N = X.n_rows;
@@ -25,9 +34,9 @@ SpResult Rigid::execute(const arma::mat& X, const arma::mat& Y) const
     double ntol = get_tol() + 10;
     double L = 0;
 
-    double L_old;
-    arma::vec P1(M), Pt1(M);
-    arma::mat PX(M, D);
+    double L_old, Np;
+    arma::vec P1(M), Pt1(M), mu_x(D), mu_y(D);
+    arma::mat PX(M, D), A(D, D);
 
     while (iter < get_max_it() &&
             ntol > get_tol() &&
@@ -35,6 +44,38 @@ SpResult Rigid::execute(const arma::mat& X, const arma::mat& Y) const
     {
         L_old = L;
         L = find_P(X, T, sigma2, get_outliers(), P1, Pt1, PX);
+
+        ntol = std::abs((L - L_old) / L);
+
+        // TODO logging
+        
+        Np = arma::sum(Pt1);
+        mu_x = X.t() * Pt1 / Np;
+        mu_y = Y.t() * P1 / Np;
+
+        A = PX.t() * Y - Np * (mu_x * mu_y.t());
+
+        arma::mat U(D, D), V(D, D);
+        arma::mat C = arma::eye<arma::mat>(D, D);
+        arma::vec sv;
+        arma::svd(U, sv, V, A);
+        arma::mat S = arma::diagmat(sv);
+
+        if (strict_rot())
+        {
+            C(D - 1, D - 1) = arma::det(U * V.t());
+        }
+
+        R = U * C * V.t();
+
+        if (use_scaling())
+        {
+             
+        }
+        else
+        {
+
+        }
 
         break;
     }
