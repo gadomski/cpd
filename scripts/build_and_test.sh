a6e21ceab4c93b3f8bc68161c02754bd0de2fec2
Pete Gadomski <pete.gadomski@gmail.com>
2014-11-21 13:10:00 -0700

    Add option to build cpd w/ 64 bit support
    When 64 bit support is enabled, the cpd library is named "cpd64" instead
    of "cpd". This is to catch mismatches at project configure time, instead
    of at link time (when the error messages are more cryptic).
    
    This change is tested in Travis via environment variables.

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -3,9 +3,16 @@ then
     export CXX="g++-4.8"
 fi
 
+if [[ "$CPD_64BIT" = true ]]
+then
+    BUILD_64BIT=ON
+else
+    BUILD_64BIT=OFF
+fi
+
 mkdir build
 cd build
-cmake .. -DBUILD_TESTS=true -DCMAKE_BUILD_TYPE=Release
+cmake .. -DBUILD_TESTS=true -DCMAKE_BUILD_TYPE=Release -DBUILD_64BIT=$BUILD_64BIT
 make
 bin/cpd-test
 sudo make install
