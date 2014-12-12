7c79b815a0b314fae52f351be6d80b1f9009724c
Pete Gadomski <pete.gadomski@gmail.com>
2014-12-12 15:04:48 -0700

    Don't check certificates on Travis for arma
    This was breaking our Travis build since we couldn't download
    armadillo's source.

--- scripts/install_armadillo.sh
+++ scripts/install_armadillo.sh
@@ -1,4 +1,4 @@
-wget https://downloads.sourceforge.net/project/arma/armadillo-4.500.1.tar.gz
+wget https://downloads.sourceforge.net/project/arma/armadillo-4.500.1.tar.gz --no-check-certificate
 tar xzf armadillo-4.500.1.tar.gz
 cd armadillo-4.500.1
 cmake .
