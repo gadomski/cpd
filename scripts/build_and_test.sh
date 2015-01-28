bd9ac67e62fa4a2d9ced1ea111771b25d1bae806
Pete Gadomski <pete.gadomski@gmail.com>
2015-01-28 10:20:00 -0600

    Add USE_ARMA_WRAPPER to the Travis build matrix
    We now have eight rows in the matrix, which is a lot, but...maybe not
    too much? Hard to say.

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -5,8 +5,20 @@ else
     BUILD_64BIT=OFF
 fi
 
+if [[ "$CPD_USE_ARMA_WRAPPER" = true ]]
+then
+    USE_ARMA_WRAPPER=ON
+else
+    USE_ARMA_WRAPPER=OFF
+fi
+
 mkdir build && cd build
-cmake .. -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -DBUILD_64BIT=$BUILD_64BIT -DBUILD_COVERAGE=$CPD_BUILD_COVERAGE && \
+cmake .. \
+        -DBUILD_TESTS=ON \
+        -DCMAKE_BUILD_TYPE=Debug \
+        -DBUILD_64BIT=$BUILD_64BIT \
+        -DBUILD_COVERAGE=$CPD_BUILD_COVERAGE \
+        -DUSE_ARMA_WRAPPER=$USE_ARMA_WRAPPER && \
     make && \
     bin/cpd-test && \
     sudo make install
