55d6e5de61f1f27703ab800563650db3d2d88054
Pete Gadomski <pete.gadomski@gmail.com>
2015-01-30 14:39:45 -0600

    Update tested armadillo version to 4.600.4
    Also fix a required version in CMakeLists.txt
    
    Fixes #25

--- scripts/install_armadillo.sh
+++ scripts/install_armadillo.sh
@@ -1,6 +1,6 @@
-wget https://downloads.sourceforge.net/project/arma/armadillo-4.500.1.tar.gz --no-check-certificate
-tar xzf armadillo-4.500.1.tar.gz
-cd armadillo-4.500.1
+wget https://downloads.sourceforge.net/project/arma/armadillo-4.600.4.tar.gz --no-check-certificate
+tar xzf armadillo-4.600.4.tar.gz
+cd armadillo-4.600.4
 cmake .
 make
 sudo make install
