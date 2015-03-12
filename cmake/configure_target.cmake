86c8acf3d17f6939608cea7a5f476e693dd10b1f
Pete Gadomski <pete.gadomski@gmail.com>
2015-03-11 21:06:21 -0500

    Ok, -Weverything was kinda silly

--- cmake/configure_target.cmake
+++ cmake/configure_target.cmake
@@ -1,7 +1,7 @@
 function(cpd_configure_target target)
     set(one_value_args NAME EXTRA_COMPILE_FLAGS)
     cmake_parse_arguments(TARGET "" "${one_value_args}" "" ${ARGN})
-    set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS " -Weverything -pedantic -Werror -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded ${TARGET_EXTRA_COMPILE_FLAGS}")
+    set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS " -Wall -pedantic -Werror -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded ${TARGET_EXTRA_COMPILE_FLAGS}")
 
     set_target_properties(${target} PROPERTIES
         VERSION ${CPD_VERSION}
