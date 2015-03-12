f87b2f55ae63099a4ca3efd430afe092ac48fda6
Pete Gadomski <pete.gadomski@gmail.com>
2015-03-12 15:17:40 -0500

    Refactor target configuration cmake

--- cmake/configure_target.cmake
+++ cmake/configure_target.cmake
@@ -1,7 +1,9 @@
 function(cpd_configure_target target)
     set(one_value_args NAME EXTRA_COMPILE_FLAGS)
     cmake_parse_arguments(TARGET "" "${one_value_args}" "" ${ARGN})
-    set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS " -Wall -pedantic -Werror -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded ${TARGET_EXTRA_COMPILE_FLAGS}")
+
+    set(compile_flags " -std=c++11 -Wall -Werror -pedantic")
+    set_property(TARGET ${target} APPEND_STRING PROPERTY COMPILE_FLAGS "${compile_flags} ${TARGET_EXTRA_COMPILE_FLAGS}")
 
     set_target_properties(${target} PROPERTIES
         VERSION ${CPD_VERSION}
