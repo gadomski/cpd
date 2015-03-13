81cf445735fabcc65ae3c60d5acb40d892734a71
Pete Gadomski <pete.gadomski@gmail.com>
2015-03-13 07:02:14 -0600

    Build RelWithDebInfo on Travis
    For code coverage

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,7 +1,7 @@
 mkdir build && cd build
 cmake .. \
         -DBUILD_TESTS=ON \
-        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
         -DBUILD_CLI=ON \
         -DARMA_64BIT_WORD=$ARMA_64BIT_WORD \
         -DBUILD_COVERAGE=$CPD_BUILD_COVERAGE && \
