149663daad0597b05fb84e6ab90b4ac98f0853fd
Pete Gadomski <pete.gadomski@gmail.com>
2015-03-12 20:27:20 -0500

    BUILD_64BIT is now ARMA_64BIT_WORD

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,16 +1,9 @@
-if [[ "$CPD_64BIT" = true ]]
-then
-    BUILD_64BIT=ON
-else
-    BUILD_64BIT=OFF
-fi
-
 mkdir build && cd build
 cmake .. \
         -DBUILD_TESTS=ON \
         -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_CLI=ON \
-        -DBUILD_64BIT=$BUILD_64BIT \
+        -DARMA_64BIT_WORD=$ARMA_64BIT_WORD \
         -DBUILD_COVERAGE=$CPD_BUILD_COVERAGE && \
     make && \
     bin/cpd-test && \
