b7fdb374e4758700b0799f587b0087924599efe4
Pete Gadomski <pete.gadomski@gmail.com>
2015-10-24 13:57:45 -0500

    Descale the rigid transformation matrix
    We were not applying the scaling and offsets to the rigid transformation
    affine matrix that is provided in the result pointer.
    
    Fixes #41

--- include/cpd/registration.hpp
+++ include/cpd/registration.hpp
@@ -67,7 +67,7 @@ public:
 
     ResultPtr run(const arma::mat& X, const arma::mat& Y) const;
     Normalization normalize(arma::mat& X, arma::mat& Y) const;
-    void denormalize(arma::mat& Y, const Normalization& normal) const;
+    virtual void denormalize(ResultPtr& result, const Normalization& normal) const;
 
     double find_P(const arma::mat& X, const arma::mat& Y, double sigma2,
                   arma::vec& P1, arma::vec& Pt1, arma::mat& PX) const;
