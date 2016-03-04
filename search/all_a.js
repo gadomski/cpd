decfe21957166888e8d8e2b6721cd54baac8b2a2
Travis CI <travis@localhost>
2016-03-04 23:46:52 +0000

    scripts/gh-pages.sh

--- search/all_a.js
+++ search/all_a.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['set_5fbeta',['set_beta',['../classcpd_1_1_nonrigid.html#ae63e386fa35349484f0ed2927f0ada5e',1,'cpd::Nonrigid']]],
+  ['set_5flambda',['set_lambda',['../classcpd_1_1_nonrigid.html#af8e4ed4e7dfaac39721cfd5f43ec5a1c',1,'cpd::Nonrigid']]],
   ['set_5fmax_5fiterations',['set_max_iterations',['../classcpd_1_1_registration.html#a7993194689341b337bcb47e24ad11d97',1,'cpd::Registration']]],
   ['set_5foutlier_5fweight',['set_outlier_weight',['../classcpd_1_1_registration.html#a74746c856243cf1bfe8b0b2b14ff8624',1,'cpd::Registration']]],
   ['set_5ftolerance',['set_tolerance',['../classcpd_1_1_registration.html#aacd63fc0d581cc5ff57fb1e9d25ef74c',1,'cpd::Registration']]]
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['beta',['beta',['../classcpd_1_1_nonrigid.html#aafba259b65cd91f9d684f993d5352c6b',1,'cpd::Nonrigid::beta() const '],['../classcpd_1_1_nonrigid.html#af384dbc97f92de746d8a827c54c1ff07',1,'cpd::Nonrigid::beta(double beta)']]]
+  ['beta',['beta',['../classcpd_1_1_nonrigid.html#aafba259b65cd91f9d684f993d5352c6b',1,'cpd::Nonrigid']]]
 ];
