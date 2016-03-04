a373adabb410c5d370e98d6205d839cc7e660846
Travis CI <travis@localhost>
2016-03-04 23:48:42 +0000

    scripts/gh-pages.sh

--- search/all_3.js
+++ search/all_3.js
@@ -1,10 +1,10 @@
 var searchData=
 [
   ['default_5fallow_5fscaling',['DEFAULT_ALLOW_SCALING',['../classcpd_1_1_rigid.html#aa442e7846a7e98a41f3fe41a5393cead',1,'cpd::Rigid']]],
-  ['default_5fbeta',['DEFAULT_BETA',['../classcpd_1_1_nonrigid.html#ad2fe4b270ccac28a4cf2af2419cb564a',1,'cpd::Nonrigid']]],
-  ['default_5flambda',['DEFAULT_LAMBDA',['../classcpd_1_1_nonrigid.html#a01ca4116fade61b47eec2a4fac180192',1,'cpd::Nonrigid']]],
+  ['default_5fbeta',['DEFAULT_BETA',['../classcpd_1_1_nonrigid.html#a3eb3f577d12146f6c3286304c2c2a204',1,'cpd::Nonrigid']]],
+  ['default_5flambda',['DEFAULT_LAMBDA',['../classcpd_1_1_nonrigid.html#a3cdff5d4451ee1e3c21befe3b5c291b0',1,'cpd::Nonrigid']]],
   ['default_5fmax_5fiterations',['DEFAULT_MAX_ITERATIONS',['../classcpd_1_1_registration.html#ad7cb74ee502d6963e54a71d09f40edb2',1,'cpd::Registration']]],
   ['default_5fno_5freflections',['DEFAULT_NO_REFLECTIONS',['../classcpd_1_1_rigid.html#a887f29bc760240d8419fb4ce6cdd1752',1,'cpd::Rigid']]],
-  ['default_5foutlier_5fweight',['DEFAULT_OUTLIER_WEIGHT',['../classcpd_1_1_registration.html#a77f8f8b54e44503035ff5cdde0ab7b50',1,'cpd::Registration']]],
-  ['default_5ftolerance',['DEFAULT_TOLERANCE',['../classcpd_1_1_registration.html#a2eb28a1ebc13cc1066757b85600d7189',1,'cpd::Registration']]]
+  ['default_5foutlier_5fweight',['DEFAULT_OUTLIER_WEIGHT',['../classcpd_1_1_registration.html#a4a070c1c79f613579b4de725799394ea',1,'cpd::Registration']]],
+  ['default_5ftolerance',['DEFAULT_TOLERANCE',['../classcpd_1_1_registration.html#a6f1baa90355b7fb8d60a4b6dde0a739a',1,'cpd::Registration']]]
 ];
