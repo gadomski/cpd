b4e46b494ce500db5fb676b252039aa6f422313d
Pete Gadomski <pete.gadomski@gmail.com>
2016-05-06 12:34:10 -0600

    Remove extraneous includes from normalization.cpp

--- src/normalization.cpp
+++ src/normalization.cpp
@@ -15,9 +15,7 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-#include "cpd/nonrigid.hpp"
 #include "cpd/normalization.hpp"
-#include "cpd/rigid.hpp"
 
 #include <iostream>
 
