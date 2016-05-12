fcc1417c796eafcc5cfeff5e1355de995e3b7461
Pete Gadomski <pete.gadomski@gmail.com>
2016-05-12 09:10:17 -0600

    Subsume registration test into utils test

--- test/utils_test.cpp
+++ test/utils_test.cpp
@@ -5,6 +5,11 @@ namespace cpd {
 
 class UtilsTest : public FishTest {};
 
+TEST_F(UtilsTest, DefaultSigma2) {
+    double sigma2 = default_sigma2(m_fish1, m_fish2);
+    EXPECT_NEAR(3.5, sigma2, 1e-8);
+}
+
 TEST_F(UtilsTest, G) {
     Matrix expected = load_test_data_matrix("fish-G.txt");
     Matrix actual = construct_affinity_matrix(m_fish2, m_fish2, 3);
