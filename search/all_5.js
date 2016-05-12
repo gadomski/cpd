0de5a35d237dfce156d547ae7096a8258810e3c3
Travis CI <travis@localhost>
2016-05-12 12:47:24 +0000

    scripts/gh-pages.sh

--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['lambda',['lambda',['../classcpd_1_1_nonrigid.html#afbf3e9bb0f720a6843eca170edfb421c',1,'cpd::Nonrigid']]],
-  ['log',['log',['../classcpd_1_1_registration.html#a1368e61e375c6376f856de20b3a46c44',1,'cpd::Registration']]]
+  ['log',['log',['../classcpd_1_1_registration.html#aeb434f2bff826a403d0cded194e69c6c',1,'cpd::Registration']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['calculate_5fprobabilities',['calculate_probabilities',['../classcpd_1_1_registration.html#aa1ece9d74172a6a4317d4bcae15a2c4b',1,'cpd::Registration']]],
-  ['compute',['compute',['../classcpd_1_1_registration.html#ab2db941503d3f5ed68808065f6d3e14a',1,'cpd::Registration::compute(const MatrixRef source, const MatrixRef target) const '],['../classcpd_1_1_registration.html#a9c3967492bd2274099dee037bd399486',1,'cpd::Registration::compute(const MatrixRef source, const MatrixRef target, double sigma2) const ']]]
+  ['compute',['compute',['../classcpd_1_1_registration.html#a7e127b702007812dd40e52f46b73e401',1,'cpd::Registration::compute(const MatrixRef source, const MatrixRef target)'],['../classcpd_1_1_registration.html#a5200a7e3e16c56719e014621adb48918',1,'cpd::Registration::compute(const MatrixRef source, const MatrixRef target, double sigma2)']]]
 ];
