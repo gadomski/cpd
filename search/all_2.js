7e131f1ce5fdbab85307d525230aa4924adae7d8
Travis CI <travis@localhost>
2016-11-20 12:07:00 +0000

    scripts/gh-pages.sh

--- search/all_2.js
+++ search/all_2.js
@@ -1,11 +1,13 @@
 var searchData=
 [
+  ['default_5fallow_5freflections',['DEFAULT_ALLOW_REFLECTIONS',['../namespacecpd.html#abd1aae56347de804a3bb5e3fd18e14a1',1,'cpd']]],
   ['default_5fbeta',['DEFAULT_BETA',['../namespacecpd.html#aad955a1cb1ff4e49ef6eeff7d7785578',1,'cpd']]],
   ['default_5fcorrespondence',['DEFAULT_CORRESPONDENCE',['../namespacecpd.html#a481ccb4650cc981d2b7312773f90b74b',1,'cpd']]],
   ['default_5flambda',['DEFAULT_LAMBDA',['../namespacecpd.html#a1e61042f2d049e263d51403a82ba9c4b',1,'cpd']]],
   ['default_5fmax_5fiterations',['DEFAULT_MAX_ITERATIONS',['../namespacecpd.html#a0551efd7d09a3f751a3332b201285b6a',1,'cpd']]],
   ['default_5fnormalize',['DEFAULT_NORMALIZE',['../namespacecpd.html#aa36624d6f9ac76830cf9db06c7bf8e31',1,'cpd']]],
   ['default_5foutliers',['DEFAULT_OUTLIERS',['../namespacecpd.html#ad01ac5a176e8aeeed80294d804bbf11d',1,'cpd']]],
+  ['default_5fscale',['DEFAULT_SCALE',['../namespacecpd.html#a0dedec7810330a1a4e4e47a246a5ad69',1,'cpd']]],
   ['default_5fsigma2',['default_sigma2',['../namespacecpd.html#af7fa64a361e61e622748ff828e0cb5f9',1,'cpd::default_sigma2(const Matrix &amp;fixed, const Matrix &amp;moving)'],['../namespacecpd.html#a9668ea7f1294377698888be196439cbc',1,'cpd::DEFAULT_SIGMA2()']]],
   ['default_5ftolerance',['DEFAULT_TOLERANCE',['../namespacecpd.html#a1c2505fb169ea031aaffaef336a4f362',1,'cpd']]],
   ['denormalize',['denormalize',['../classcpd_1_1_nonrigid.html#a528f92a20db0fe9919de8eb6b3b64fcf',1,'cpd::Nonrigid::denormalize()'],['../classcpd_1_1_rigid.html#a36c9df1de17b1cb0ec6229bba521c2ff',1,'cpd::Rigid::denormalize()']]]
--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['l',['l',['../structcpd_1_1_probabilities.html#aa7dcb64c047451647a44ad0f81ca44b7',1,'cpd::Probabilities']]],
   ['logger_5fname',['LOGGER_NAME',['../namespacecpd.html#a8051fdf3762fd08b08c54a4f2cdd00d1',1,'cpd']]],
   ['logging_2ehpp',['logging.hpp',['../logging_8hpp.html',1,'']]]
 ];
