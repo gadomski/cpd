bd424683831e366144efe3e6397022854f02a693
Pete Gadomski <pete.gadomski@gmail.com>
2019-03-05 07:46:11 -0700

    Update Doxygen version to 1.8.15

--- scripts/travis-install-doxygen.sh
+++ scripts/travis-install-doxygen.sh
@@ -3,9 +3,9 @@
 
 set -ex
 
-DOXYGEN_VER=doxygen-1.8.11
+DOXYGEN_VER=doxygen-1.8.15
 DOXYGEN_TAR=${DOXYGEN_VER}.linux.bin.tar.gz
-DOXYGEN_URL="http://ftp.stack.nl/pub/users/dimitri/${DOXYGEN_TAR}"
+DOXYGEN_URL="http://doxygen.nl/files/${DOXYGEN_TAR}"
 
 mkdir -p $HOME/local/bin
 wget -O - "${DOXYGEN_URL}" | tar xz -C ${TMPDIR-/tmp} ${DOXYGEN_VER}/bin/doxygen
